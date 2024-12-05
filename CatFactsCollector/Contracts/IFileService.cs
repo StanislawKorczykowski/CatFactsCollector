a51543cd0429fb68dffbff6caf831ef30dfb9be0
s24645 <s24645@pjwstk.edu.pl>
2024-12-05 16:36:05 +0100

    Add method to save multiple cat facts  to file

--- CatFactsCollector/Contracts/IFileService.cs
+++ CatFactsCollector/Contracts/IFileService.cs
@@ -5,5 +5,6 @@ namespace CatFactsCollector.Contracts;
 public interface IFileService
 {
     void AppendToFile(CatFact fact);
+    void AppendToFile(CatFactsDto facts);
 
 }
--- CatFactsCollector/Services/FileService.cs
+++ CatFactsCollector/Services/FileService.cs
@@ -5,9 +5,17 @@ namespace CatFactsCollector.Services;
 
 public class FileService(IConfiguration configuration) : IFileService
 {
-    public  void AppendToFile(CatFact catFact)
-    { 
+    public void AppendToFile(CatFact catFact)
+    {
         using var writer = new StreamWriter(configuration["FilePath"]!, true);
         writer.WriteLine(catFact.Fact);
+    }
+    public void AppendToFile(CatFactsDto catFactsDto)
+    {
+        using var writer = new StreamWriter(configuration["FilePath"]!, true);
+        foreach (var catFact in catFactsDto.Facts!)
+        {
+            writer.WriteLine(catFact.Fact);
+        }
     }
 }
