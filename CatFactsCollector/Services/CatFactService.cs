cd6d4e7cd4a59c017425b0c02112a9674cb39474
s24645 <s24645@pjwstk.edu.pl>
2024-12-05 16:18:58 +0100

    Update services to use appsettings

--- CatFactsCollector/Services/CatFactService.cs
+++ CatFactsCollector/Services/CatFactService.cs
@@ -4,11 +4,13 @@ using Newtonsoft.Json;
 
 namespace CatFactsCollector.Services;
 
-public class CatFactService(HttpClient httpClient) : ICatFactService
+public class CatFactService(HttpClient httpClient, IConfiguration configuration) : ICatFactService
 {
     public async Task<CatFact?> GetCatFactAsync()
     {
-        var response = await httpClient.GetStringAsync("https://catfact.ninja/fact");
+        var uriBuilder = new UriBuilder(configuration["BaseURL"]!);
+        uriBuilder.Path += "fact";
+        var response = await httpClient.GetStringAsync(uriBuilder.Uri);
         var catFact = JsonConvert.DeserializeObject<CatFact>(response);
         return catFact;
     }
--- CatFactsCollector/Services/FileService.cs
+++ CatFactsCollector/Services/FileService.cs
@@ -3,11 +3,11 @@ using CatFactsCollector.Models;
 
 namespace CatFactsCollector.Services;
 
-public class FileService : IFileService
+public class FileService(IConfiguration configuration) : IFileService
 {
     public  void AppendToFile(CatFact catFact)
     { 
-        using var writer = new StreamWriter("catFacts.txt", true);
+        using var writer = new StreamWriter(configuration["FilePath"]!, true);
         writer.WriteLine(catFact.Fact);
     }
 }
