a6c58a34f3c03417a328bb4d35a866855c0c1a24
s24645 <s24645@pjwstk.edu.pl>
2024-12-05 15:52:54 +0100

    Add cat fact controller

---
+++ CatFactsCollector/Controllers/CatFactController.cs
@@ -0,0 +1,17 @@
+﻿using CatFactsCollector.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatFactsCollector.Controllers;
+
+[ApiController]
+[Route("catFactController")]
+public class CatFactController(ICatFactService catFactService) : ControllerBase
+{
+    [HttpGet]
+    [Route("/fact")]
+    public async Task<IActionResult> GetCatFact()
+    {
+        var catFact = await catFactService.GetCatFactAsync();
+        return Ok(catFact);
+    }
+}
