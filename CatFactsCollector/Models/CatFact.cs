595a62c9d43849697006a98c1e7c337f6a7549a0
s24645 <s24645@pjwstk.edu.pl>
2024-12-05 15:50:42 +0100

    Add model for cat facts

---
+++ CatFactsCollector/Models/CatFact.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatFactsCollector.Models;
+
+public class CatFact
+{
+    [JsonPropertyName("fact")]
+    public string? Fact { get; set; }
+    
+    [JsonPropertyName("length")]
+    public string? Length { get; set; }
+}
